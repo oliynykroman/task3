9cb73b6bbf9a4b75a3bd84d339034a67d1119679
Roman Oliynyk <oliynykroma@live.com>
2019-09-23 21:17:37 +0300

    made routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,21 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./pages/quiz/quiz.module').then(mod => mod.QuizModule)
+  },
+  {
+    path: '**',
+    redirectTo: '404',
+    pathMatch: 'full'
+  },
+  {
+    path: '404',
+    loadChildren: () => import('./pages/page-not-found/page-not-found.module').then(mod => mod.PageNotFoundModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
