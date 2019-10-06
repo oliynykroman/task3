dec5649571f29745f90896274f3438a1618193b8
Roman Oliynyk <oliynykroma@live.com>
2019-10-06 10:52:13 +0300

    modified model with answers

--- src/app/models/quiz-answers.model.ts
+++ src/app/models/quiz-answers.model.ts
@@ -1,12 +1,6 @@
 export class QuizAnswers {
-    question_id: number;
-    right_answer: string;
-    user_answer:string;
-    status: string;
-    constructor(question_id: number, right_answer: string, user_answer: string, status: string){
-        this.question_id = question_id || 0;
-        this.right_answer = right_answer || '';
-        this.user_answer = user_answer || '';
-        this.status = status || '';
-    }
+    id: number;
+    rightAnswer: string;
+    userAnswer:string;
+    questionStatus: string;
 }
