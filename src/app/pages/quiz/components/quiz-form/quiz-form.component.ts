6342a6fbd93b6c651fc6db5a71de9c96d233d53e
Roman Oliynyk <oliynykroma@live.com>
2019-10-07 22:39:22 +0300

    xsa

--- src/app/pages/quiz/components/quiz-form/quiz-form.component.ts
+++ src/app/pages/quiz/components/quiz-form/quiz-form.component.ts
@@ -16,13 +16,8 @@ export class QuizFormComponent implements OnInit, OnDestroy {
   public formFields: FormGroup;
   public payLoad = '';
   public id: number = 0;
-<<<<<<< HEAD
-  public question: QuizStructure = new QuizStructure;
-  public filteredQuestion: QuizStructure = new QuizStructure;
-=======
   public questionsArray: QuizStructure[] = [];
   public question: any;
->>>>>>> a7b4ecb233824719eaa65105aab642bbd5a78f77
 
   private subscription: Subscription;
 
@@ -34,7 +29,6 @@ export class QuizFormComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit() {
-<<<<<<< HEAD
     this.id = +this.route.snapshot.paramMap.get('id');
     this.subscription = this.quizService.getQuestions(this.id).subscribe(data => this.question = data);
     this.formFields = this.quizTransformService.toFormGroup(this.question);
@@ -42,14 +36,6 @@ export class QuizFormComponent implements OnInit, OnDestroy {
 
   onSubmit() {
     this.payLoad = JSON.stringify(this.formFields.value);
-=======
-this.subscription = this.quizService.QuizInfo$.subscribe(data => this.id = id)
-  
-  }
-
-  onSubmit() {
-    this.payLoad = JSON.stringify(this.form.value);
->>>>>>> a7b4ecb233824719eaa65105aab642bbd5a78f77
     console.log(this.payLoad);
   }
   ngOnDestroy() {
